8b90530b23bb54d781f6b576668c0ea315e5344e
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-09-07 15:20:03 +0530

    c++

---
+++ functions.cpp/q9.cpp
@@ -0,0 +1,19 @@
+// Write a function to generate the nth Fibonacci number using recursion.
+#include <iostream> 
+using namespace std ;
+int fib(int n){
+    if (n==0) return 0 ;
+    else if (n==1) return 1 ; 
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+    
+    
+}
+int main (){
+    int n ; 
+    cout << " enter the n " << endl; 
+    cin >> n ; 
+   cout << fib(n);
+
+}
